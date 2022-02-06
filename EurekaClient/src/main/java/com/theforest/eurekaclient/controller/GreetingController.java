f5d86d292e9a7dc06106fd3a39a55b3a481b1769
aguselnikov <aguselnikov@fil-it.ru>
2022-02-06 20:39:16 +0300

    add log

--- EurekaClient/src/main/java/com/theforest/eurekaclient/controller/GreetingController.java
+++ EurekaClient/src/main/java/com/theforest/eurekaclient/controller/GreetingController.java
@@ -2,12 +2,14 @@ package com.theforest.eurekaclient.controller;
 
 import com.netflix.discovery.EurekaClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class GreetingController {
@@ -17,9 +19,9 @@ public class GreetingController {
     @Value("${spring.application.name}")
     private String appName;
 
-
     @RequestMapping("/greeting")
     public String greeting() {
+        log.info("I'm working");
         return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
     }
 }
